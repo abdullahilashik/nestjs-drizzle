29ed5dd95ce105fb631eed016edcac169a3e1cdd
Abdullahil Ashik Md Arefin <abdullah.ashik.arefin@gmail.com>
2026-08-24 19:08:35 +0600

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [
-    DrizzleModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    UsersModule,
-    PostModule
+  imports: [    
+    ConfigModule.forRoot({ isGlobal: true })
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
